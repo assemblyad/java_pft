b7a01508b935435bce7865f8d12d92c5c7750700
assemblyad <61529304+assemblyad@users.noreply.github.com>
2020-08-21 23:23:32 -0600

    Exercise 3 Completed, see comment below
    Задание №3: Подключить тестовый фреймворк и сделать первые тесты.
    Подключить к модулю sandbox тестовый фреймворк TestNG и сделать несколько тестов для класса Point, которые проверяют, что расстояние между точками вычисляется правильно.
    
    ---
    
    После выполнения задания сохраните код в удаленный репозиторий и в качестве ответа на задание сообщите тренеру ссылку на последний коммит, а также можете написать любые дополнительные комментарии и вопросы.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Exercise2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Exercise2.java
@@ -2,8 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class Exercise2 {
   public static void main(String[] args) {
-    Point p1= new Point(4,4);
-    Point p2= new Point(2,2);
+    Point p1= new Point(2,5);
+    Point p2= new Point(5,5);
     System.out.println("The distance between two point is "+ p1.distance(p2));
     }
 }
