f0dd21a3b668bbd8b9c699d76c7aef4b82192293
assemblyad <61529304+assemblyad@users.noreply.github.com>
2020-08-21 20:41:38 -0600

    Exercice 2 , Class point and distance calculation
    Implemented:
    
    Задание №2: Потренироваться использовать функции, классы, объекты и методы
    1. Создать класс Point для представления точек на двумерной плоскости. Объекты этого класса должны содержать два атрибута, которые соответствуют координатам точки на плоскости.
    
    2. Создать функцию
    public static double distance(Point p1, Point p2)
    которая вычисляет расстояние между двумя точками. Для вычисления квадратного корня можно использовать функцию Math.sqrt
    
    3. Сделать запускаемый класс, то есть содержащий функцию
    public static void main(String[] args) {...}
    и при помощи него убедиться, что функция вычисления расстояния между точками действительно работает. Результат вычисления выводить на экран и контролировать визуально.
    
    4. Реализовать то же самое (вычисление расстояния между двумя точками) при помощи метода в классе Point, и добавить в созданный в предыдущем пункте запускаемый класс примеры использования метода вместо ранее созданной функции.
    
    ---
    
    После выполнения задания сохраните код в удаленный репозиторий и в качестве ответа на задание сообщите тренеру ссылку на последний коммит, а также можете написать любые дополнительные комментарии и вопросы.

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Exercise2.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+public class Exercise2 {
+  public static void main(String[] args) {
+    Point p1= new Point(4,4);
+    Point p2= new Point(2,2);
+    System.out.println("The distance between two point is "+ p1.distance(p2));
+    }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  int x;
+  int y;
+  public Point (int x, int y) {
+    this.x=x;
+    this.y=y;
+  }
+  public double distance(Point p2) {
+    int x= this.x-p2.x;
+    int y= this.y-p2.y;
+    //return Math.sqrt((((p1.x -p2.x)*2) + ((p1.y-p2.y)*2)));
+    return Math.sqrt(x*x+y*y);
+  }
+
+}
