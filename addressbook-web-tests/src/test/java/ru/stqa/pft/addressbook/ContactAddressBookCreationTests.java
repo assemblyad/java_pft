d96022c3fe83df0f8aa53a8bb1d6d0c763e58642
assemblyad <61529304+assemblyad@users.noreply.github.com>
2020-08-28 13:01:54 -0600

    Задание №4:
    Задание №4: Сделать тесты для создания групп и контактов в адресной книге.
    The contact named ContactAddressBookRecord

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactAddressBookCreationTests.java
@@ -0,0 +1,127 @@
+package ru.stqa.pft.addressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ContactAddressBookCreationTests {
+  private WebDriver wb;
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    wb = new FirefoxDriver();
+    wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wb.get("http://localhost/addressbook/");
+    login("admin", "secret");
+
+  }
+
+  @Test
+  public void testContactAddressBookCreation() throws Exception {
+    gotoHome();
+    initContactAddressRecord();
+    fillContactAddressBookRecord(new ContactAddressBookRecord("First name", "Middle_name", "Last_name", "Nickname", "Title", "Company", "Address", "Home", "Mobile", "Work", "Fax", "E-mail", "E-mail2", "E-mail3", "Homepage", "Group name", "Greenwood Village", "Home", "Notes", "5", "April", "1975", "5", "April", "1980"));
+    submitContactAddressBookRecord();
+    gotoHomePage();
+    logout();
+  }
+
+  private void logout() {
+    wb.findElement(By.linkText("Logout")).click();
+  }
+
+  private void gotoHomePage() {
+    wb.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitContactAddressBookRecord() {
+    wb.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void fillContactAddressBookRecord(ContactAddressBookRecord addressBookRecord) {
+    wb.findElement(By.name("firstname")).clear();
+    wb.findElement(By.name("firstname")).sendKeys(addressBookRecord.getFirstName());
+    wb.findElement(By.name("middlename")).clear();
+    wb.findElement(By.name("middlename")).sendKeys(addressBookRecord.getMiddleName());
+    wb.findElement(By.name("lastname")).clear();
+    wb.findElement(By.name("lastname")).sendKeys(addressBookRecord.getLastName());
+    wb.findElement(By.name("nickname")).sendKeys(addressBookRecord.getNickname());
+    wb.findElement(By.name("title")).clear();
+    wb.findElement(By.name("title")).sendKeys(addressBookRecord.getTitle());
+    wb.findElement(By.name("company")).clear();
+    wb.findElement(By.name("company")).sendKeys(addressBookRecord.getCompany());
+    wb.findElement(By.name("address")).clear();
+    wb.findElement(By.name("address")).sendKeys(addressBookRecord.getAddress());
+    wb.findElement(By.name("home")).clear();
+    wb.findElement(By.name("home")).sendKeys(addressBookRecord.getHome());
+    wb.findElement(By.name("mobile")).clear();
+    wb.findElement(By.name("mobile")).sendKeys(addressBookRecord.getMobile());
+    wb.findElement(By.name("work")).clear();
+    wb.findElement(By.name("work")).sendKeys(addressBookRecord.getWork());
+    wb.findElement(By.name("fax")).clear();
+    wb.findElement(By.name("fax")).sendKeys(addressBookRecord.getFax());
+    wb.findElement(By.name("email")).clear();
+    wb.findElement(By.name("email")).sendKeys(addressBookRecord.getEmail());
+    wb.findElement(By.name("email2")).clear();
+    wb.findElement(By.name("email2")).sendKeys(addressBookRecord.getEmail2());
+    wb.findElement(By.name("email3")).clear();
+    wb.findElement(By.name("email3")).sendKeys(addressBookRecord.getEmail3());
+    wb.findElement(By.name("homepage")).clear();
+    wb.findElement(By.name("homepage")).sendKeys(addressBookRecord.getHomepage());
+    new Select(wb.findElement(By.name("bday"))).selectByVisibleText(addressBookRecord.getBday());
+    new Select(wb.findElement(By.name("bmonth"))).selectByVisibleText(addressBookRecord.getBmonth());
+    wb.findElement(By.name("byear")).clear();
+    wb.findElement(By.name("byear")).sendKeys(addressBookRecord.getByear());
+    new Select(wb.findElement(By.name("aday"))).selectByVisibleText(addressBookRecord.getAday());
+    new Select(wb.findElement(By.name("amonth"))).selectByVisibleText(addressBookRecord.getAmonth());
+    wb.findElement(By.name("ayear")).clear();
+    wb.findElement(By.name("ayear")).sendKeys(addressBookRecord.getAyear());
+    new Select(wb.findElement(By.name("new_group"))).selectByVisibleText(addressBookRecord.getGroupName());
+    wb.findElement(By.name("address2")).clear();
+    wb.findElement(By.name("address2")).sendKeys(addressBookRecord.getAddress2());
+    wb.findElement(By.name("phone2")).clear();
+    wb.findElement(By.name("phone2")).sendKeys(addressBookRecord.getHome1());
+    wb.findElement(By.name("notes")).clear();
+    wb.findElement(By.name("notes")).sendKeys(addressBookRecord.getNotes());
+  }
+
+  private void initContactAddressRecord() {
+    wb.findElement(By.linkText("add new")).click();
+  }
+
+  private void gotoHome() {
+    wb.findElement(By.linkText("home")).click();
+  }
+
+  private void login(String username, String password) {
+    wb.findElement(By.name("user")).sendKeys(username);
+    wb.findElement(By.name("pass")).sendKeys(password);
+    wb.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wb.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wb.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wb.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactAddressBookRecord.java
@@ -0,0 +1,157 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactAddressBookRecord {
+  private final String firstName;
+  private final String middleName;
+  private final String lastName;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String home;
+  private final String mobile;
+  private final String work;
+  private final String fax;
+  private final String email;
+  private final String email2;
+  private final String email3;
+  private final String homepage;
+  private final String groupName;
+  private final String address2;
+  private final String home1;
+  private final String notes;
+  private final String bday;
+  private final String bmonth;
+  private final String byear;
+  private final String aday;
+  private final String amonth;
+  private final String ayear;
+
+  public ContactAddressBookRecord(String firstName, String middleName, String lastName, String nickname, String title, String company, String address, String home, String mobile, String work, String fax, String email, String email2, String email3, String homepage, String groupName, String address2, String home1, String notes, String bday, String bmonth, String byear, String aday, String amonth, String ayear) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.home = home;
+    this.mobile = mobile;
+    this.work = work;
+    this.fax = fax;
+    this.email = email;
+    this.email2 = email2;
+    this.email3 = email3;
+    this.homepage = homepage;
+    this.groupName = groupName;
+    this.address2 = address2;
+    this.home1 = home1;
+    this.notes = notes;
+    this.bday = bday;
+    this.bmonth = bmonth;
+    this.byear = byear;
+    this.aday = aday;
+    this.amonth = amonth;
+    this.ayear = ayear;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHome() {
+    return home;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getWork() {
+    return work;
+  }
+
+  public String getFax() {
+    return fax;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getEmail2() {
+    return email2;
+  }
+
+  public String getEmail3() {
+    return email3;
+  }
+
+  public String getHomepage() {
+    return homepage;
+  }
+
+  public String getGroupName() {
+    return groupName;
+  }
+
+  public String getAddress2() {
+    return address2;
+  }
+
+  public String getHome1() {
+    return home1;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public String getBday() {
+    return bday;
+  }
+
+  public String getBmonth() {
+    return bmonth;
+  }
+
+  public String getByear() {
+    return byear;
+  }
+
+  public String getAday() {
+    return aday;
+  }
+
+  public String getAmonth() {
+    return amonth;
+  }
+
+  public String getAyear() {
+    return ayear;
+  }
+}
