76af00a0834ba1f1b1cfd09574c5cc5ec62e7871
assemblyad <61529304+assemblyad@users.noreply.github.com>
2020-08-29 12:55:04 -0600

    Лекция 2.7. Выделяем специализированные классы-помощники

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,63 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+  WebDriver wd;
+  private SessionHelper sessionHelper;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+
+  public void init() {
+    wd = new FirefoxDriver();
+    /*wd = new ChromeDriver(); */
+//      driver = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin","secret");
+  }
+
+
+  public void returnToGroupPage() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  public void stop() {
+    wd.quit();
+  }
+
+  public boolean isElementPresent(By by) {
+    try {
+      groupHelper.wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  public boolean isAlertPresent() {
+    try {
+      groupHelper.wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
+  }
+
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTests extends TestBase {
+
+  @Test
+  public void testGroupCreation() throws Exception {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("Group name", "header", "footer"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnGroupCreation();
+    app.returnToGroupPage();
+  }
+
+
+}
