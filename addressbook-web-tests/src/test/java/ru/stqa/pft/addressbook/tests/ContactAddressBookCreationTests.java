0658f53f79ac3b53b54b019bf8f16dee274836f3
assemblyad <61529304+assemblyad@users.noreply.github.com>
2020-09-14 19:17:05 -0600

    Лекция 4.9. Лямбда-выражения (анонимные функции) и элементы функционального программирования
    Лекция 4.9. Лямбда-выражения (анонимные функции) и элементы функционального программирования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressBookCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressBookCreationTests.java
@@ -14,26 +14,26 @@ public class ContactAddressBookCreationTests extends TestBase {
     app.getContactAddressBookRecordHelper().gotoHome();
     List<ContactAddressBookRecordData> before = app.getContactAddressBookRecordHelper().getContactAddressBookRecordList();
     app.getContactAddressBookRecordHelper().initContactAddressRecord();
-    ContactAddressBookRecordData contactAddressBookRecordData = new ContactAddressBookRecordData("First_name", "Middle_name", "Last_name", "Nickname", "Title", "Company", "Address", "Home", "Mobile", "Work", "Fax", "E-mail", "E-mail2", "E-mail3", "Homepage", "Group name", "Greenwood Village", "Home", "Notes", "5", "April", "1975", "5", "April", "1980");
-    app.getContactAddressBookRecordHelper().fillContactAddressBookRecord(contactAddressBookRecordData,true);
+    ContactAddressBookRecordData contactAddressBookRecordData = new ContactAddressBookRecordData("First_name_45", "Middle_name", "Last_name_45", "Nickname", "Title", "Company", "Address", "Home", "Mobile", "Work", "Fax", "E-mail", "E-mail2", "E-mail3", "Homepage", "Group name", "Greenwood Village", "Home", "Notes", "5", "April", "1975", "5", "April", "1980");
+    app.getContactAddressBookRecordHelper().fillContactAddressBookRecord(contactAddressBookRecordData, true);
     app.getContactAddressBookRecordHelper().submitContactAddressBookRecord();
     app.getContactAddressBookRecordHelper().returnedHomePage();
     List<ContactAddressBookRecordData> after = app.getContactAddressBookRecordHelper().getContactAddressBookRecordList();
-    Assert.assertEquals(after.size(),before.size()+1);
-
+    Assert.assertEquals(after.size(), before.size() + 1);
+/*
     int max=0;
       for (ContactAddressBookRecordData contactAddressBookRecord: after){
         if (contactAddressBookRecord.getId()>max) {
           max=contactAddressBookRecord.getId();
         }
       }
-
-    contactAddressBookRecordData.setId(max);
+*/
+    contactAddressBookRecordData.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contactAddressBookRecordData);
 
 
-    Assert.assertEquals(new HashSet<>(before),new HashSet<>(after));
-    Assert.assertEquals(before,after);
+    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
+    Assert.assertEquals(before, after);
   }
 
 }
