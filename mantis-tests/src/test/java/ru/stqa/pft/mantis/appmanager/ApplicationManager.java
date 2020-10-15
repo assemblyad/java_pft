afaf97fdba4d6246a7de2369c73286976ba3a10e
assemblyad <61529304+assemblyad@users.noreply.github.com>
2020-10-14 18:40:26 -0600

    Лекция 8.3. Прямое взаимодействие с сервером по протоколу HTTP
    opened quetion to team

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.HttpSessionId;
 
 import java.io.File;
 import java.io.FileReader;
@@ -47,4 +48,12 @@ public class ApplicationManager {
 
   public void stop() { wd.quit();  }
 
+  public HttpSession newSession(){
+    return new HttpSession(this);
+  }
+
+
+  public String getProperty(String key){
+    return properties.getProperty(key);
+  }
 }
