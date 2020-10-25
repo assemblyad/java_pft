ffdf815179837f458be8d381d0beac204bf9debf
assemblyad <61529304+assemblyad@users.noreply.github.com>
2020-10-25 13:01:42 -0600

    Updated application manager to have parameter WINDOWS intead of Win10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -52,7 +52,7 @@ public class ApplicationManager {
     }else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "Win10")));
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "WINDOWS")));
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
 
